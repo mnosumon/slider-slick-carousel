7658185a77f2d3273d69ecae03d7ba9fff922019
mnosumon <mdnuruddin378350@gmail.com>
2024-12-11 00:53:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MultiItemCarousel from "./componets/MultiItemCarosel";
+
+const App = () => {
+  return (
+    <div>
+      <MultiItemCarousel />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/componets/MultiData.jsx
@@ -0,0 +1,31 @@
+// import image1 from "./assets/carouselImage1.jpg";
+// import image2 from "./assets/carouselImage2.jpg";
+// import image3 from "./assets/carouselImage3.jpg";
+// import image4 from "./assets/carouselImage4.jpg";
+// import image5 from "./assets/carouselImage5.jpg";
+// import image6 from "./assets/carouselImage6.jpg";
+
+import multicar1 from "../assets/multicar1.jpg";
+import multicar2 from "../assets/multicar2.jpg";
+import multicar3 from "../assets/multicar3.jpg";
+import multicar4 from "../assets/multicar4.jpg";
+import multicar5 from "../assets/multicar5.jpg";
+import multicar6 from "../assets/multicar6.jpg";
+import multicar7 from "../assets/multicar7.jpg";
+import multicar8 from "../assets/multicar8.jpg";
+import multicar9 from "../assets/multicar9.jpg";
+import multicar10 from "../assets/multicar10.jpg";
+
+// export const data = [image1, image2, image3, image4, image5, image6];
+export const multiData = [
+  multicar1,
+  multicar10,
+  multicar2,
+  multicar3,
+  multicar4,
+  multicar5,
+  multicar9,
+  multicar6,
+  multicar7,
+  multicar8,
+];
