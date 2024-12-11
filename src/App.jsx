48106fce0aeb4d5f2ee39f374436efa78e0617c7
mnosumon <mdnuruddin378350@gmail.com>
2024-12-11 23:48:58 +0600

    testmonial slider done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import MultiItemCarousel from "./componets/MultiItemCarosel";
+import Testimonial from "./componets/testMonial/TestMonial";
 
 const App = () => {
   return (
-    <div>
-      <MultiItemCarousel />
+    <div className="conatiner">
+      <div className="multiSlide">
+        <MultiItemCarousel />
+      </div>
+      <div className="multiSlide">
+        <Testimonial />
+      </div>
     </div>
   );
 };
